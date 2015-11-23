35500de2a514f4f0e8386f61df969b5747db3d09
Iris de Vries <Iris@wcw-stud-145-109-26-225.wireless.uva.nl>
2015-11-23 16:02:30 +0100

    update

--- Homework/week-5/format.py
+++ Homework/week-5/format.py
@@ -2,7 +2,7 @@ import csv
 import json
 import re
 f = open('data_population.csv', 'r')
-reader = csv.reader(f, delimiter=' ',skipinitialspace=True)
+reader = csv.reader(f, delimiter='\t',skipinitialspace=True)
 
 # make empty list
 aList = []
@@ -10,23 +10,14 @@ aList = []
 # modify data
 for row in reader:
         tempor = []
-        print row[1]
-
-        #re.split('\t+', row)
         countries = row[0]
-        #print countries
+        values = row[1]
+        print countries
+        print values
 
-        # sdate = ''
-        # for char in date:
-        #     if len(sdate) == 4 or len(sdate) == 7:
-        #         sdate += '/'
-        #     sdate += char
-        # tempor.append(sdate)
-
-        # # get temperature
-        # temp = row[1:2][0]
-        # tempor.append(temp)
-        # aList.append(tempor)
+        tempor.append(countries)
+        tempor.append(values)
+        aList.append(tempor)
 
 with open('formatted_population.txt', 'w') as fp:
-	json.dump(countries, fp)
+	json.dump(aList, fp)
